2a77d8eadf3f6a33ae17f43ecc82183319fa8a28
Reazul87 <hasanreazul87@gmail.com>
2025-11-09 13:49:23 +0600

    GET_PRODUCTS_API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to GlobalNexus !");
@@ -22,12 +26,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const productsDB = client.db("ProductsDB");
+    const productsCollection = productsDB.collection("Products");
+
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     //     await client.close();
