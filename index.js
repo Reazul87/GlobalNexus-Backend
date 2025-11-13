58f1d011d6c4ad7f6f781759ad2c248773feb842
Reazul87 <hasanreazul87@gmail.com>
2025-11-13 23:27:32 +0600

    DELETE_IMPORT_PRODUCTS

--- index.js
+++ index.js
@@ -164,6 +164,12 @@ async function run() {
       res.send(result);
     }); ///Complete
 
+    app.delete("/import-product/:id", verifyIdToken ,async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await importsCollection.deleteOne(query);
+      res.send(result);
+    }); ///Complete
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
